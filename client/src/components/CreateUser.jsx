89c3043004019528ab239b8d914453b3f7af4205
BrandonDEv <vixfgit@gmail.com>
2024-06-27 01:42:08 +0300

    stylizing createUser component

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateUser = () => {
+    const [userInfo, setUserInfo] = useState({name:'', email:''})
+
+    const handleChange =(e) => {
+        const {name, value} = e.target
+        setUserInfo(userInfo => ({...userInfo,[name]:value}))
+    }
+
+    console.log(userInfo);
     return (
-        <div>
-            Create user
+        <div className='form-container'>
+                <h3>New User</h3>
+                <form  onSubmit={null}>
+                    
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input id='name' type="text" name='name' onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input id='email' type="text" name='email' onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <button>Save</button>
+                    </div>
+
+                </form>
         </div>
     );
 }
