a8adf8789b6da99fcff9a2e0a896ba0b4f225137
BrandonDEv <vixfgit@gmail.com>
2024-06-27 02:21:27 +0300

    adding toast

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const CreateUser = () => {
     const [userInfo, setUserInfo] = useState({name:'', email:''})
-
+    const navigate = useNavigate()
     const handleChange =(e) => {
         const {name, value} = e.target
         setUserInfo(userInfo => ({...userInfo,[name]:value}))
@@ -11,10 +14,13 @@ const CreateUser = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         await axios.post('http://localhost:8000/api/createUser', userInfo).then(res => {
-            console.log(res.data);
-        }).catch(err => console.log(err.message))
+            
+        }).catch(err => toast.error(err.message))
+        navigate('/')
     }
+
     return (
+        <>
         <div className='form-container'>
                 <h3>New User</h3>
                 <form  onSubmit={handleSubmit}>
@@ -33,6 +39,9 @@ const CreateUser = () => {
 
                 </form>
         </div>
+        <ToastContainer/>
+        </>
+
     );
 }
 
