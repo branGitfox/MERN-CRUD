56c94866140b0ab999be91ae9dc746738eaa154a
BrandonDEv <vixfgit@gmail.com>
2024-06-27 01:55:26 +0300

    stylizing listUser component

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FaTrash, FaUpload } from 'react-icons/fa';
 const UserList = () => {
     return (
-        <div>
-            <h1>List of USERS</h1>
+        <div className='list-container'>
+            <h3>List of User</h3>
+            <div className="table-container">
+                <table>
+                  <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                        <tr>
+                            <td>Ravomanana</td>
+                            <td>Brandon Fidelin</td>
+                            <td className='actions'><Link to="/delete/1"><FaTrash fill='red'/></Link><Link to="/delete/1"><FaUpload fill='green'/></Link></td>
+                        </tr>
+                  </tbody>
+                </table>
+            </div>
         </div>
     );
 }
